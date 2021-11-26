5b8f06a27fe1e5fac61213c0dcbec1a9cff51bf7
Alex <alexandre.bourdeau@mailbox.org>
2021-11-26 17:10:14 -0500

    feat: route for /doc

--- app/routes/router.py
+++ app/routes/router.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Blueprint, render_template
 
 router = Blueprint("router", __name__)
@@ -6,3 +7,8 @@ router = Blueprint("router", __name__)
 @router.route('/')
 def home():
     return render_template('index.html')
+
+
+@router.route('/doc')
+def doc():
+    return flask.Response(status=501)
