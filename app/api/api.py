2f5dc73b09b5cd4cfa4ccaa20d81115cedc5b1f4
Alex <alexandre.bourdeau@mailbox.org>
2021-11-27 23:55:55 -0500

    feat: installations api route filters results with borough arg

--- app/api/api.py
+++ app/api/api.py
@@ -1,10 +1,7 @@
 # Native and installed modules
-from flask import Blueprint, jsonify
-from utils.shared import db
-import json
+from flask import Blueprint, jsonify, request
 
 # Custom modules
-from model.arrondissement import Arrondissement, ArrondissementModel
 from model.glissade import Glissade, GlissadeModel
 from model.installation_aquatique import InstallationAquatique, InstallationAquatiqueModel
 from model.patinoire import Patinoire, PatinoireModel
@@ -16,14 +13,22 @@ api = Blueprint("api", __name__, url_prefix="/api")
 def installations():
     slides = Glissade.query.all()
     slide_model = GlissadeModel(many=True)
-    serialized_slides = slide_model.dump(slides)
-
     aquatic_installations = InstallationAquatique.query.all()
     aquatic_installations_model = InstallationAquatiqueModel(many=True)
-    serialized_aquatic_installations = aquatic_installations_model.dump(aquatic_installations)
-
     skating_rinks = Patinoire.query.all()
     ice_rinks_model = PatinoireModel(many=True)
+
+    arr_filter = request.args.get('arrondissement')
+    if arr_filter is not None:
+        slides = Glissade.query \
+            .filter(Glissade.arrondissement.has(nom=arr_filter)).all()
+        aquatic_installations = InstallationAquatique.query \
+            .filter(InstallationAquatique.arrondissement.has(nom=arr_filter)).all()
+        skating_rinks = Patinoire.query \
+            .filter(Patinoire.arrondissement.has(nom=arr_filter)).all()
+
+    serialized_slides = slide_model.dump(slides)
+    serialized_aquatic_installations = aquatic_installations_model.dump(aquatic_installations)
     serialized_ice_rinks = ice_rinks_model.dump(skating_rinks)
 
     return jsonify({
