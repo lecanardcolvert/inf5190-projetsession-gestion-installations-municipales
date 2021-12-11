16addf14dcd5361a622a8557fac489d1b3dc216e
Alex <alexandre.bourdeau@mailbox.org>
2021-12-11 17:03:05 -0500

    fix: add missing dependencies

---
+++ app/static/js/fill_facility_name_dropdown.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+fill_name_dropdown_menu();
+
+// ===== FILL DROPDOWN MENU - SEARCH BY NAME =====
+function fill_name_dropdown_menu() {
+    var name = $("#facility-name").val();
+    var method = 'get';
+    var url = '/api/installations-noms';
+
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Content-type', 'application/json');
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                xhr.response.forEach(name =>
+                    $("#facility-name").append(
+                    '<option value="' + name + '">' + name + '</option>'
+                    ));
+            } else {
+                console.log(xhr.response);
+            }
+        }
+    };
+    xhr.send();
+}
+
+
+
+});
