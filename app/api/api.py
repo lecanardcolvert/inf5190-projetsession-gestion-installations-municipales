aead4fa35b1abfef0571c19b015ec388887d1079
Axel Sally <be991046@ens.uqam.ca>
2021-12-11 14:27:02 -0500

    doc: reformat code doc

--- app/api/api.py
+++ app/api/api.py
@@ -1,35 +1,27 @@
 # Native and installed modules
 from dicttoxml import dicttoxml
 from flask import Blueprint, jsonify, request, Response
+from sqlalchemy.orm import contains_eager
 from xml.dom.minidom import parseString
-import flask
-import json
-import jsonschema
-from jsonschema import validate
 
 # Custom modules
-from model.arrondissement import Arrondissement, ArrondissementModel
-from model.subscriber import Subscriber, SubscriberModel
-from sqlalchemy.orm import contains_eager
+from model.arrondissement import Arrondissement
 from model.glissade import Glissade, GlissadeModel
 from model.installation_aquatique import (
     InstallationAquatique,
     InstallationAquatiqueModel,
 )
 from model.patinoire import Patinoire, PatinoireModel
-from sqlalchemy import exc
-from utils.shared import db
 
 api = Blueprint("api", __name__, url_prefix="/api")
 
 
 def _get_installations():
     """
-    Returns the list of facilities for later usage by an API.
-    Return:
-        The aquatic installations
-        The ice rinks
-        The slides
+    Fetches the facilities in the database and return the list.
+
+    Returns:
+    tuple -- The aquatic installations, the ice rinks, the slides
     """
     aquatic_installations = InstallationAquatique.query.all()
     ice_rinks = Patinoire.query.all()
@@ -37,37 +29,20 @@ def _get_installations():
     return aquatic_installations, ice_rinks, slides
 
 
-def _get_json_schema():
-    with open("user_schema.json", "r") as file:
-        schema = json.load(file)
-    return schema
-
-
-def _validate_json(schema_filename, json_data):
-    with open(schema_filename, "r") as file:
-        schema = json.load(file)
-
-    try:
-        validate(instance=json_data, schema=schema)
-    except jsonschema.exceptions.ValidationError as err:
-        return False
-    return True
-
-
 @api.route("/installations", methods=["GET"])
 def installations():
     """
-    Returns the list of facilities in the JSON format with an optional filter
-    by borough.
+    Return the list of facilities in the JSON format.
+    Optional filter by borough using 'arrondissement' keyword arg.
 
-    Keyword argument:
+    Keyword arguments:
     arrondissement -- The exact name of the borough of the facility.
 
-    Return:
-    The list of facilities in JSON format.
+    Returns:
+    json -- The list of facilities in JSON format
     """
-
     aquatic_installations, ice_rinks, slides = _get_installations()
+
     arr_filter = request.args.get("arrondissement")
     if arr_filter is not None:
         slides = Glissade.query.filter(
@@ -83,41 +58,26 @@ def installations():
     aquatic_installation_model = InstallationAquatiqueModel(many=True)
     ice_rink_model = PatinoireModel(many=True)
     slide_model = GlissadeModel(many=True)
-    serialized_aquatic_installations = aquatic_installation_model.dump(
-        aquatic_installations
-    )
+    serialized_aquatic = aquatic_installation_model.dump(aquatic_installations)
     serialized_ice_rinks = ice_rink_model.dump(ice_rinks)
     serialized_slides = slide_model.dump(slides)
-    serialized_aquatic_installations = aquatic_installation_model.dump(
-        aquatic_installations
-    )
-    serialized_ice_rinks = ice_rink_model.dump(ice_rinks)
 
     return jsonify(
         {
             "glissades": serialized_slides,
-            "installations_aquatiques": serialized_aquatic_installations,
+            "installations_aquatiques": serialized_aquatic,
             "patinoires": serialized_ice_rinks,
         }
     )
 
 
-@api.route("/arrondissements", methods=["GET"])
-def boroughs():
-    borough_list = Arrondissement.query.all()
-    borough_model = ArrondissementModel(many=True)
-    serialized_boroughs = borough_model.dump(borough_list)
-    return jsonify(serialized_boroughs)
-
-
 def _facilities_updated_2021():
     """
-    Returns the list of facilities updated in 2021, in ascending sorting order, for later usage by an API.
+    Fetches the facilities in the database. Orders them in ascending sorting
+    order. Returns the list.
 
-    Return:
-        The aquatic installations updated in 2021, in ascending order (name)
-        The ice rinks updated in 2021, in ascending order (name)
-        The slides updated in 2021, in ascending order (name)
+    Returns:
+    tuple -- The aquatic installations, the ice rinks, the slides
     """
     year = "2021"
 
@@ -144,32 +104,26 @@ def _facilities_updated_2021():
     aquatic_installation_model = InstallationAquatiqueModel(many=True)
     ice_rink_model = PatinoireModel(many=True)
     slide_model = GlissadeModel(many=True)
-    serialized_aquatic_installations = aquatic_installation_model.dump(
-        aquatic_installations
-    )
+    serialized_aquatic = aquatic_installation_model.dump(aquatic_installations)
     serialized_ice_rinks = ice_rink_model.dump(skating_rinks)
     serialized_slides = slide_model.dump(slides)
-    return (
-        serialized_aquatic_installations,
-        serialized_ice_rinks,
-        serialized_slides,
-    )
+    return serialized_aquatic, serialized_ice_rinks, serialized_slides
 
 
 @api.route("/installations-maj-2021", methods=["GET"])
 def facilities_updated_2021():
     """
-    Returns the list of facilities updated in 2021 in the JSON format.
+    Return the list of facilities updated in 2021 in the JSON format.
 
-    Return:
-    The list of facilities updated in 2021 in JSON format.
+    Returns:
+    json -- The list of facilities updated in 2021
     """
-
     aquatic_installations, ice_rinks, slides = _facilities_updated_2021()
+
     return jsonify(
         {
             "glissades": slides,
-            "installations_aquatiques": aquatic_installations,
+            "installations_aqutiques": aquatic_installations,
             "patinoires": ice_rinks,
         }
     )
@@ -180,8 +134,8 @@ def facilities_updated_2021_xml():
     """
     Returns the list of facilities updated in 2021 in the XML format.
 
-    Return:
-    The list of facilities updated in 2021 in XML format.
+    Returns:
+    xml -- The list of facilities updated in 2021
     """
     aquatic_installations, ice_rinks, slides = _facilities_updated_2021()
     xml_data = [
@@ -200,38 +154,3 @@ def facilities_updated_2021_xml():
     pretty_xml_data = parsed_xml_data.toprettyxml()
 
     return Response(pretty_xml_data, mimetype="application/xml")
-
-
-@api.route("/abonnement", methods=["POST"])
-def subscribe():
-    json_schema_path = flask.current_app.root_path + "/schemas/subscribe.json"
-    request_data = request.get_json()
-
-    if _validate_json(json_schema_path, request_data):
-        subscriber = Subscriber(
-            request_data["full_name"],
-            request_data["email"],
-            request_data["boroughs_to_follow"],
-        )
-
-        try:
-            db.session.add(subscriber)
-            db.session.commit()
-            subscriber_model = SubscriberModel()
-            serialized_subscriber = subscriber_model.dump(subscriber)
-            return jsonify(serialized_subscriber), 201
-        except exc.SQLAlchemyError as err:
-            return (
-                jsonify(
-                    {
-                        "error": "Une erreur est survenue lors de l'ajout dans la base de données."
-                    }
-                ),
-                500,
-            )
-
-    else:
-        return (
-            jsonify({"error": "Les données fournies ne sont pas valides."}),
-            400,
-        )
