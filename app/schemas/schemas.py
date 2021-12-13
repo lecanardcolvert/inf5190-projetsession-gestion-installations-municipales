f265b6644106e32765b4cc11cf8f3a44b99a178c
Axel Sally <be991046@ens.uqam.ca>
2021-12-13 01:46:19 -0500

    feat(D3): add delete and update for all installations

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -17,3 +17,59 @@ update_playground_slide_schema = {
     },
     "additionalProperties": False,
 }
+
+update_ice_rink_schema = {
+    "type": "object",
+    "required": [
+        "nom",
+        "arrondissement_id",
+        "date_heure",
+        "ouvert",
+        "deblaye",
+        "arrose",
+        "resurface",
+    ],
+    "properties": {
+        "nom": {"type": "string", "minLength": 1},
+        "arrondissement_id": {
+            "type": "number",
+            "minimum": 0,
+        },
+        "date_heure": {
+            "type": "string",
+            "format": "full-date",
+            "minLength": 10,
+        },
+        "ouvert": {"type": "number", "minimum": 0},
+        "deblaye": {"type": "number", "minimum": 0},
+        "arrose": {"type": "number", "minimum": 0},
+        "resurface": {"type": "number", "minimum": 0},
+    },
+    "additionalProperties": False,
+}
+
+update_aquatic_installation_schema = {
+    "type": "object",
+    "required": [
+        "nom",
+        "arrondissement_id",
+        "type",
+        "adresse",
+        "propriete",
+        "gestion",
+        "equipement",
+    ],
+    "properties": {
+        "nom": {"type": "string", "minLength": 1},
+        "arrondissement_id": {
+            "type": "number",
+            "minimum": 0,
+        },
+        "type": {"type": "string", "minLength": 1},
+        "adresse": {"type": "string", "minLength": 1},
+        "propriete": {"type": "string", "minLength": 1},
+        "gestion": {"type": "string", "minLength": 1},
+        "equipement": {"type": "string", "minLength": 1},
+    },
+    "additionalProperties": False,
+}
