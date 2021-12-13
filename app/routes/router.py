baf40ab3faf76a0151541db966a4ea038c84ca7d
Axel Sally <be991046@ens.uqam.ca>
2021-12-12 23:04:46 -0500

    feat: add api doc to frontend

--- app/routes/router.py
+++ app/routes/router.py
@@ -1,20 +1,14 @@
 # Native and installed modules
-import os
 from flask import Blueprint, current_app, render_template
 
 router = Blueprint("router", __name__)
 
 
-@router.route('/')
+@router.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@router.route('/doc')
+@router.route("/doc")
 def doc():
-    input_raml = os.path.join(current_app.root_path, 'api', 'api.raml')
-    output_html = os.path.join(current_app.root_path,
-                               current_app.static_folder, 'html',
-                               'api-doc.html')
-    os.system("raml2html " + input_raml + " > " + output_html)
-    return current_app.send_static_file('html/api-doc.html')
+    return render_template("api-doc.html")
